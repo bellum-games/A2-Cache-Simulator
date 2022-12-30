22c674f4b1c358d81439d13559d4514adb17a705
Bucur Dan-Alexandru <79925845+BellumGames@users.noreply.github.com>
2022-12-30 16:16:47 +0200

    Remade the GUI, folder structure, added traces, remade the history for GUI

--- A2/A2.cs
+++ A2/A2.cs
@@ -5,9 +5,9 @@ namespace A2
 {
     public partial class A2 : Form
     {
-        private int SIZE_IC, SIZE_DC, N_PEN, V, D, NR_PORT;
-        private int IC, DC, IBS, IRmax, FR, NR_REG;
-        private int MissRateIC, MissRateDC, PercentageIBS_Empty, Influence_IRmax, OptimalREG_Number;
+        private Dictionary<string, List<Tuple<char, uint, uint>>> allTraceData = new Dictionary<string, List<Tuple<char, uint, uint>>>();
+        private int latenta, NR_PORT, FR, IRmax, IBS, N_PEN, NR_REG, FR_IC, SIZE_IC, FR_DC, SIZE_DC; //Parameters for simulation
+        private int MissRateIC, MissRateDC, PercentageIBS_Empty, Influence_IRmax, OptimalREG_Number; //This should be outputed in results.csv maybe? Cosmin any ideas?
 
         //date out:
         //rata de procesare (nr instr raportat la nr cicli de executie)
@@ -43,27 +43,35 @@ namespace A2
                 return;
             }
             SetComboForFuture();
+            ReadTraces();
             Simulate();
             WriteResults();
         }
 
+        private void btnEXIT_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
         private void IOneeded()
         {
-            if (!File.Exists("history.txt"))
+            if (!File.Exists(@"..\..\..\output\history.txt"))
             {
-                File.Create("history.txt");
+                File.Create(@"..\..\..\output\history.txt");
             }
-            if (!File.Exists("results.csv"))
+            if (!File.Exists(@"..\..\..\output\results.csv"))
             {
-                File.Create("results.csv");
+                File.Create(@"..\..\..\output\results.csv");
             }
         }
 
         private bool ValidParameters()
         {
+            if(FR_IC != FR || FR_DC != FR)
+                return false;
             if (IRmax > FR)
                 return false;
-            if (IC > IBS || DC > IBS)
+            if (SIZE_IC > IBS || SIZE_DC > IBS)
                 return false;
             if (NR_REG > IRmax)
                 return false;
@@ -73,14 +81,19 @@ namespace A2
         private bool SafeStart()
         {
             if (
-                comboSIZE_IC.SelectedIndex > -1 &&
-                comboSIZE_DC.SelectedIndex > -1 &&
+                latentaUpDown.Value >= 0 &&
+                (uniportRadio.Checked == true || biportRadio.Checked == true) &&
+                comboFR.SelectedIndex > -1 &&
                 comboIRmax.SelectedIndex > -1 &&
                 comboIBS.SelectedIndex > -1 &&
-                comboFR.SelectedIndex > -1 &&
                 comboN_PEN.SelectedIndex > -1 &&
                 comboNR_REG.SelectedIndex > -1 &&
-                comboNR_PORT.SelectedIndex > -1)
+
+                comboFR_IC.SelectedIndex > -1 &&
+                comboSIZE_IC.SelectedIndex > -1 &&
+                comboFR_DC.SelectedIndex > -1 &&
+                comboSIZE_DC.SelectedIndex > -1
+            )
                 return true;
             else
                 return false;
@@ -88,39 +101,49 @@ namespace A2
 
         private void SetEnviroment() 
         {
-            SIZE_IC = int.Parse((string)comboSIZE_IC.SelectedItem);
-            SIZE_DC = int.Parse((string)comboSIZE_DC.SelectedItem);
+            latenta = (int)latentaUpDown.Value;
+            if (uniportRadio.Checked == true && biportRadio.Checked == false)
+                NR_PORT = 1;
+            else if (uniportRadio.Checked == false && biportRadio.Checked == true)
+                NR_PORT = 2;
+            FR = int.Parse((string)comboFR.SelectedItem);
             IRmax = int.Parse((string)comboIRmax.SelectedItem);
             IBS = int.Parse((string)comboIBS.SelectedItem);
-            FR = int.Parse((string)comboFR.SelectedItem);
             N_PEN = int.Parse((string)comboN_PEN.SelectedItem);
             NR_REG = int.Parse((string)comboNR_REG.SelectedItem);
-            NR_PORT = int.Parse((string)comboNR_PORT.SelectedItem);
+            FR_IC = int.Parse((string)comboFR_IC.SelectedItem);
+            SIZE_IC = int.Parse((string)comboSIZE_IC.SelectedItem);
+            FR_DC = int.Parse((string)comboFR_DC.SelectedItem);
+            SIZE_DC = int.Parse((string)comboSIZE_DC.SelectedItem);
         }
 
         private void SetComboForFuture()
         {
-            using (StreamWriter sw = new StreamWriter("history.txt"))
+            using (StreamWriter sw = new StreamWriter(@"..\..\..\output\history.txt"))
             {
-                //sw.WriteLine($"comboIC {comboIC.SelectedIndex}");
-                //sw.WriteLine($"comboDC {comboDC.SelectedIndex}");
-                sw.WriteLine($"comboSIZE_IC {comboSIZE_IC.SelectedIndex}");
-                sw.WriteLine($"comboSIZE_DC {comboSIZE_DC.SelectedIndex}");
+                sw.WriteLine($"latentaUpDown {latentaUpDown.Value}");
+                if(uniportRadio.Checked == true && biportRadio.Checked == false)
+                    sw.WriteLine($"uniportRadio_biportRadio {1}");
+                else if(uniportRadio.Checked == false && biportRadio.Checked == true)
+                    sw.WriteLine($"uniportRadio_biportRadio {2}");
+
+                sw.WriteLine($"comboFR {comboFR.SelectedIndex}");
                 sw.WriteLine($"comboIRmax {comboIRmax.SelectedIndex}");
                 sw.WriteLine($"comboIBS {comboIBS.SelectedIndex}");
-                sw.WriteLine($"comboFR {comboFR.SelectedIndex}");
-                //sw.WriteLine($"comboV {comboV.SelectedIndex}");
-                //sw.WriteLine($"comboD {comboD.SelectedIndex}");
                 sw.WriteLine($"comboN_PEN {comboN_PEN.SelectedIndex}");
                 sw.WriteLine($"comboNR_REG {comboNR_REG.SelectedIndex}");
-                sw.WriteLine($"comboNR_PORT {comboNR_PORT.SelectedIndex}");
+
+                sw.WriteLine($"comboFR_IC {comboFR_IC.SelectedIndex}");
+                sw.WriteLine($"comboSIZE_IC {comboSIZE_IC.SelectedIndex}");
+                sw.WriteLine($"comboFR_DC {comboFR_DC.SelectedIndex}");
+                sw.WriteLine($"comboSIZE_DC {comboSIZE_DC.SelectedIndex}");
                 sw.Close();
             }
         }
 
         private void SetComboFromHistory() 
         {
-            string allText = File.ReadAllText("history.txt");
+            string allText = File.ReadAllText(@"..\..\..\output\history.txt");
             if (!allText.Equals(string.Empty)) 
             {
                 string[] rows = allText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
@@ -131,21 +154,35 @@ namespace A2
                     int value = int.Parse(values[1]);
                     switch (i)
                     {
-                        case 0: comboSIZE_IC.SelectedIndex = value; break;
-                        case 1: comboSIZE_DC.SelectedIndex = value; break;
-                        case 2: comboIRmax.SelectedIndex = value; break;
-                        case 3: comboIBS.SelectedIndex = value; break;
-                        case 4: comboFR.SelectedIndex = value; break;
+                        case 0: latentaUpDown.Value = value; break;
+                        case 1:
+                            if(value == 1) 
+                            {
+                                uniportRadio.Checked = true;
+                                biportRadio.Checked = false;
+                            }
+                            else if(value == 2) 
+                            {
+                                uniportRadio.Checked = false;
+                                biportRadio.Checked = true;
+                            }
+                            break;
+                        case 2: comboFR.SelectedIndex = value; break;
+                        case 3: comboIRmax.SelectedIndex = value; break;
+                        case 4: comboIBS.SelectedIndex = value; break;
                         case 5: comboN_PEN.SelectedIndex = value; break;
                         case 6: comboNR_REG.SelectedIndex = value; break;
-                        case 7: comboNR_PORT.SelectedIndex = value; break;
+
+                        case 7: comboFR_IC.SelectedIndex = value; break;
+                        case 8: comboSIZE_IC.SelectedIndex = value; break;
+                        case 9: comboFR_DC.SelectedIndex = value; break;
+                        case 10: comboSIZE_DC.SelectedIndex = value; break;
                     }
                     i++;
                 }
             }
         }
 
-        private Dictionary<string, List<Tuple<char, uint, uint>>> allTraceData = new Dictionary<string, List<Tuple<char, uint, uint>>>();
         private void ReadTrace(FileInfo file)
         {
             List<Tuple<char, uint, uint>> traceData = new List<Tuple<char, uint, uint>>();
@@ -155,7 +192,7 @@ namespace A2
             foreach(string row in rows) 
             {
                 string[] bits = row.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                for (int i = 0; i < bits.Length; i += 3) 
+                for (int i = 0; i < bits.Length; i += 3)
                 {
                     char instruction = bits[i][0];
                     uint addressOne = uint.Parse(bits[i + 1]);
@@ -178,25 +215,21 @@ namespace A2
 
         private void Simulate()
         {
-            ReadTraces();
+            /*
             string s = string.Empty;
             foreach (var item in allTraceData) 
             {
                 s += item.Key.ToString() + " " + item.Value.Count.ToString() + Environment.NewLine;
             }
             textBoxConsole.Text = s;
+            */
         }
 
         private void WriteResults()
         {
-            using (StreamWriter sw = new StreamWriter("results.csv"))
+            using (StreamWriter sw = new StreamWriter(@"..\..\..\output\results.csv"))
             {
-                MissRateIC = 0;
-                MissRateDC = 0;
-                PercentageIBS_Empty = 0;
-                Influence_IRmax = 0;
-                OptimalREG_Number = 0;
-
+                /*
                 sw.WriteLine(",,,,");
                 sw.WriteLine(",,,,");
                 sw.WriteLine(",,Data,Result");
@@ -205,6 +238,7 @@ namespace A2
                 sw.WriteLine($",,Percentage of prefetch buffer that is empty,{PercentageIBS_Empty}");
                 sw.WriteLine($",,Influence of IRmax on performance,{Influence_IRmax}");
                 sw.WriteLine($",,Optimal number of registers,{OptimalREG_Number}");
+                */
                 sw.Close();
             }
         }
